ba6f5e4ca19964776f4c2f5d734542f2afe8c76a
ibra <ibrahimsm@msmk.university>
2025-04-03 14:09:14 +0200

    buscador de paises

--- src/app/pages/paises/paises.component.ts
+++ src/app/pages/paises/paises.component.ts
@@ -1,25 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-paises',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './paises.component.html',
   styleUrl: './paises.component.css'
 })
 export class PaisesComponent {
   paisBuscar: string= "";
   paisRecibido: any;
+  error=null;
   getPaisesWithFetch(pais: string) {
-    fetch(`https://restcountries.com/v3.1/capital/${pais}`)
+    fetch(`https://restcountries.com/v3.1/name/${pais}?fulltext=false`)
       .then(response => response.json())
       .then(data => {
       this.paisRecibido = data[ 0]
+      this.error=null; 
         console.log('Fetch:', data)
       })
       .catch(error => {
-     
+        this.error=error;
+        this.paisRecibido=null;
         console.error('Error con Fetch:', error)
       });
   }
